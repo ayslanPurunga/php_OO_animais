1cf74fe9603c4be13f688829bc3264b4be337168
luksk <lukskill@hotmail.com>
2020-10-07 00:24:38 -0300

    Classe Dinossauro extends Animal

---
+++ src/ClassesEntidades/Dinossauro.php
@@ -0,0 +1,11 @@
+<?php 
+
+declare(strict_types=1);
+
+namespace Alfa\ClassesEntidades;
+
+class Dinossauro extends Animal {
+    public function emiteSom(): string {
+        return "Ghhhhhrrrrraaaaaa";
+    }
+}
